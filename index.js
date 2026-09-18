93f015851640a36d3cf199bd9fd5ecd2ac219cc3
Bruno de Lima Caires <caires.bruno@escola.pr.gov.br>
2026-09-18 14:31:30 +0400

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function somar(){
+  let n1 = documento.getElementById('numero1').value ;
+  let n1 = documento.getElementById('numero2').value ;
+  document.getElementById('resultado').innerText = 'Resultado: $(n1 + n2)'
+    }
