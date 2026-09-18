3210fb3d49f4ba685387fc6df8c7d4c523e74225
Bruno de Lima Caires <caires.bruno@escola.pr.gov.br>
2026-09-18 14:45:09 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function somar(){
-  let n1 = documento.getElementById('numero1').value ;
-  let n1 = documento.getElementById('numero2').value ;
+  let n1 = Number(document.getElementById('numero1').value ;
+  let n2 = Number(document.getElementById('numero2').value ;
   document.getElementById('resultado').innerText = 'Resultado: $(n1 + n2)'
     }
+
+function subtrair(){
+  let n1 = Number(document.getElementById('numero1').value');
+  let n2 = Number(document.getElementById('numero2').value');
+  document.getElementById('resultado').innerText = 'Resultado: $(n1 - n2)'
+}
